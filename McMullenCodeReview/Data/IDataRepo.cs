066951cb5fd3f7f6872242375c46ec5696f36d01
David McMullen <dmcmullen44@gmail.com>
2022-05-31 14:17:49 -0700

    Added dependency injection and dummy data implementation

---
+++ McMullenCodeReview/Data/IDataRepo.cs
@@ -0,0 +1,14 @@
+﻿using McMullenCodeReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace McMullenCodeReview.Data
+{
+    public interface IDataRepo
+    {
+        IEnumerable<AlbumModel> GetAlbums();
+        IEnumerable<PhotoModel> GetPhotosByAlbumId(int AlbumID);
+    }
+}
---
+++ McMullenCodeReview/Data/MockDataRepo.cs
@@ -0,0 +1,32 @@
+﻿using McMullenCodeReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace McMullenCodeReview.Data
+{
+    public class MockDataRepo : IDataRepo
+    {
+        public IEnumerable<AlbumModel> GetAlbums()
+        {
+            var albums = new List<AlbumModel>
+            {
+                new AlbumModel{UserId=12, Id=1, Title="Test Pictures" },
+                new AlbumModel{UserId=12, Id=2, Title="Test Album 2" },
+            };
+            return albums;
+        }
+
+        public IEnumerable<PhotoModel> GetPhotosByAlbumId(int AlbumID)
+        {
+            var photos = new List<PhotoModel>
+            { 
+               new PhotoModel{ AlbumID=1, Id=1, Title="Photo 1",Url = "https://via.placeholder.com/600/92c952", thumbnailUrl="https://via.placeholder.com/150/92c952"},
+               new PhotoModel{ AlbumID=1, Id=2, Title="Photo 2",Url = "https://via.placeholder.com/600/771796", thumbnailUrl="https://via.placeholder.com/150/771796"},
+               new PhotoModel{ AlbumID=1, Id=3, Title="Photo 3",Url = "https://via.placeholder.com/600/24f355", thumbnailUrl="https://via.placeholder.com/150/24f355"}
+            };
+            throw new NotImplementedException();
+        }
+    }
+}
