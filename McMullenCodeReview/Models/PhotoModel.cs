4ceb0ffde1379a1c038b136016422327a743c66a
David McMullen <dmcmullen44@gmail.com>
2022-05-31 12:57:50 -0700

    Added Data models

---
+++ McMullenCodeReview/Models/PhotoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace McMullenCodeReview.Models
+{
+    public class PhotoModel
+    {
+        public int AlbumID { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string thumbnailUrl { get; set; }
+    }
+}
